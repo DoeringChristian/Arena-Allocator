de6adf2a82a78983541d8e1c0bd0a765896eca5f
Christian Döring <doering.christian.2001@gmx.de>
2022-03-21 21:54:23 +0100

    Added SArena

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 
 pub mod arena;
+pub mod sarena;
 
 pub use arena::*;
+pub use sarena::*;
 
 #[cfg(test)]
 mod tests {
--- src/sarena.rs
+++ src/sarena.rs
@@ -1,26 +1,19 @@
 
-use std::{marker::PhantomData, ops::{Index, IndexMut}};
+use std::marker::PhantomData;
 
-///
-/// Cell of an Arena.
-///
-#[derive(Debug)]
-pub enum ArenaCell<T>{
-    Allocated{val: T, generation: usize},
-    Freed{next: Option<usize>, generation: usize},
-}
+use crate::*;
 
 ///
 /// An index referring to an index and epoch in an Arena.
 ///
 #[derive(Debug, PartialEq, Eq)]
-pub struct ArenaIdx<T>{
+pub struct SArenaIdx<T>{
     index: usize,
     generation: usize,
     _ty: PhantomData<T>,
 }
 
-impl<T> ArenaIdx<T>{
+impl<T> SArenaIdx<T>{
     pub fn new(index: usize, generation: usize) -> Self{
         Self{
             index,
@@ -40,179 +33,96 @@ impl<T> ArenaIdx<T>{
     }
 }
 
-// Have to implement copy and clone myselfe because of generic.
-impl<T> Clone for ArenaIdx<T>{
+impl<T> Clone for SArenaIdx<T>{
     #[inline]
     fn clone(&self) -> Self {
         *self
     }
 }
 
-impl<T> Copy for ArenaIdx<T>{}
+impl<T> Copy for SArenaIdx<T>{}
 
-///
-/// An Arena allocator that keeps track of freed cells in a Vec.
-///
-/// # Example
-///
-///```rust
-///
-/// use gen_arena::arena::*;
-///
-/// let mut arena = Arena::new();
-///
-/// let i0 = arena.insert(0);
-/// let i1 = arena.insert(1);
-///
-/// assert_eq!(*arena.get(i0).unwrap(), 0);
-/// assert_eq!(*arena.get(i1).unwrap(), 1);
-///
-/// arena.remove(i1);
-///
-/// assert_eq!(arena.get(i1), None);
-///
-/// let i2 = arena.insert(2);
-///
-/// assert_eq!(*arena.get(i2).unwrap(), 2);
-/// assert_eq!(arena.get(i1), None);
-///
-///```
-///
-#[derive(Debug)]
-pub struct Arena<T>{
-    cells: Vec<ArenaCell<T>>,
+pub struct SArena<T, const N: usize>{
+    cells: [ArenaCell<T>; N],
     freed: Option<usize>,
     num: usize,
 }
 
-impl<T> Arena<T>{
-
+impl<T, const N: usize> SArena<T, N>{
     ///
-    /// Creates an empty Arena.
+    /// Creates a new empty SArena.
     ///
     ///```rust
     /// use gen_arena::*;
     ///
-    /// let arena = Arena::<i32>::new();
+    /// let arena = SArena::<i32, 100>::new();
     ///```
     ///
     pub fn new() -> Self{
-        Self{
-            cells: Vec::new(),
-            freed: None,
-            num: 0,
-        }
-    }
-
-    ///
-    /// Creates an emty Arena with capacity.
-    ///
-    /// ```rust
-    ///
-    /// use gen_arena::*;
-    ///
-    /// let arena = Arena::<i32>::with_capacity(10);
-    ///
-    /// assert_eq!(arena.capacity(), 10);
-    ///
-    /// ```
-    ///
-    pub fn with_capacity(cap: usize) -> Self{
-        Self{
-            cells: Vec::with_capacity(cap),
-            freed: None,
-            num: 0,
-        }
-    }
-
-    ///
-    /// Clears the arena and resets the list of Freed cells.
-    ///
-    /// ```rust
-    ///
-    /// use gen_arena::*;
-    ///
-    /// let mut arena = Arena::new();
-    ///
-    /// let i1 = arena.insert(1);
-    /// let i2 = arena.insert(2);
-    ///
-    /// arena.clear();
-    ///
-    /// assert_eq!(arena.get(i1), None);
-    /// assert_eq!(arena.get(i2), None);
-    ///
-    /// ```
-    ///
-    pub fn clear(&mut self){
-        let len = self.cells.len();
-        for (i, cell) in self.cells.iter_mut().enumerate(){
-            match cell{
-                ArenaCell::Allocated{val: _, generation} => {
-                    *cell = ArenaCell::Freed{
-                        generation: *generation + 1,
-                        next: if i < len-1 {Some(i+1)} else{None},
-                    }
-                },
-                ArenaCell::Freed{next: _, generation} => {
-                    *cell = ArenaCell::Freed{
-                        generation: *generation,
-                        next: if i < len-1 {Some(i+1)} else{None},
-                    }
-                }
-            }
-        }
-        self.num = 0;
-    }
-
-    ///
-    /// Tries to insert into Arena.
-    /// Returns val as Err if failed.
-    ///
-    pub fn try_insert(&mut self, val: T) -> Result<ArenaIdx<T>, T>{
-        match self.freed{
-            Some(i) => {
-                if let ArenaCell::Freed{next, generation} = self.cells[i]{
-                    self.freed = next;
-                    self.cells[i] = ArenaCell::Allocated{
-                        val,
-                        generation,
-                    };
-                    self.num += 1;
-                    Ok(ArenaIdx{
-                        index: i,
-                        generation,
-                        _ty: PhantomData,
-                    })
+        let mut i = 0;
+        let cells: [ArenaCell<T>; N] = [(); N].map(|()|{
+            let ret = {
+                if i < N -1{
+                    ArenaCell::Freed{next: Some(i +1), generation: 0}
                 }
                 else{
-                    Err(val)
+                    ArenaCell::Freed{next: None, generation: 0}
                 }
-            }
-            None => {
-                self.cells.push(ArenaCell::Allocated{
-                    generation: 0,
-                    val,
-                });
-                self.num += 1;
-                Ok(ArenaIdx{
-                    index: self.cells.len() -1,
-                    generation: 0,
-                    _ty: PhantomData,
-                })
+            };
+            i += 1;
+            ret
+        });
+        
+        Self{
+            cells,
+            freed: Some(0),
+            num: 0,
+        }
+    }
+
+    ///
+    /// Tries to insert a value into the Arena.
+    /// Unlike Arena::try_insert this does not need a mut ref 
+    /// because the array stays in the same place all the time.
+    ///
+    #[must_use]
+    pub fn try_insert(&self, val: T) -> Result<SArenaIdx<T>, T>{
+
+        // SAFETY: 
+        // - Insertion abborts if cell is iccupied hence only freed cells are affected.
+        // - The memory location of cells does not change on insertion unlike Vec.
+        unsafe{
+            let selfp = (self as *const Self) as *mut Self;
+            match self.freed{
+                Some(i) => {
+                    if let ArenaCell::Freed{next, generation} = self.cells[i]{
+                        (*selfp).freed = next;
+                        (*selfp).cells[i] = ArenaCell::Allocated{
+                            val,
+                            generation,
+                        };
+                        (*selfp).num += 1;
+                        Ok(SArenaIdx::new(i, generation))
+                    }
+                    else{
+                        Err(val)
+                    }
+                }
+                None => Err(val)
             }
         }
     }
 
     ///
     /// Inserts a new element into the Arena.
+    /// Panics if it is full.
     ///
     /// # Example:
     ///
     /// ```rust 
     /// use gen_arena::*;
     ///
-    /// let mut arena = Arena::new();
+    /// let arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     /// let i2 = arena.insert(2);
@@ -223,7 +133,7 @@ impl<T> Arena<T>{
     /// ```
     ///
     #[must_use]
-    pub fn insert(&mut self, val: T) -> ArenaIdx<T>{
+    pub fn insert(&self, val: T) -> SArenaIdx<T>{
         match self.try_insert(val){
             Ok(index) => index,
             Err(_val) => panic!("Insertion not successfull."),
@@ -233,7 +143,7 @@ impl<T> Arena<T>{
     ///
     /// Removes the cell from the arena and increaces its generation.
     ///
-    pub fn remove(&mut self, index: ArenaIdx<T>){
+    pub fn remove(&mut self, index: SArenaIdx<T>){
         if let ArenaCell::Allocated{val: _, generation} = &self.cells[index.index]{
             self.cells[index.index] = ArenaCell::Freed{
                 next: self.freed,
@@ -260,7 +170,7 @@ impl<T> Arena<T>{
     /// ```rust
     /// use gen_arena::*;
     ///
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     ///
@@ -272,7 +182,7 @@ impl<T> Arena<T>{
     ///
     /// ```
     ///
-    pub fn get(&self, index: ArenaIdx<T>) -> Option<&T>{
+    pub fn get(&self, index: SArenaIdx<T>) -> Option<&T>{
         if let ArenaCell::Allocated{val, generation} = &self.cells[index.index]{
             if *generation == index.generation{
                 Some(val)
@@ -289,6 +199,24 @@ impl<T> Arena<T>{
     ///
     /// Returns an optional reference to a cell with any generation.
     ///
+    /// Get N optional references to N indices in the arena.
+    ///
+    /// ```rust
+    /// use gen_arena::*;
+    ///
+    /// let mut arena = SArena::<_, 100>::new();
+    ///
+    /// let i1 = arena.insert(1);
+    /// let i2 = arena.insert(2);
+    ///
+    /// let res = arena.getn([i1, i2]);
+    ///
+    /// assert_eq!(*res[0].unwrap(), 1);
+    /// assert_eq!(*res[1].unwrap(), 2);
+    ///
+    /// ```
+    ///
+    ///
     pub fn get_any(&self, index: usize) -> Option<&T>{
         if let ArenaCell::Allocated{val, generation: _} = &self.cells[index]{
             Some(val)
@@ -304,7 +232,7 @@ impl<T> Arena<T>{
     /// ```rust
     /// use gen_arena::*;
     ///
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     /// let i2 = arena.insert(2);
@@ -316,8 +244,8 @@ impl<T> Arena<T>{
     ///
     /// ```
     ///
-    pub fn getn<const N: usize>(&self, indices: [ArenaIdx<T>; N]) -> [Option<&T>; N]{
-        let mut ret = [None; N];
+    pub fn getn<const M: usize>(&self, indices: [SArenaIdx<T>; M]) -> [Option<&T>; M]{
+        let mut ret = [None; M];
 
         for (i, index) in indices.iter().enumerate(){
             ret[i] = self.get(*index);
@@ -331,7 +259,7 @@ impl<T> Arena<T>{
     /// ```rust
     /// use gen_arena::*;
     ///
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     ///
@@ -347,7 +275,7 @@ impl<T> Arena<T>{
     ///
     /// ```
     ///
-    pub fn get_mut(&mut self, index: ArenaIdx<T>) -> Option<&mut T>{
+    pub fn get_mut(&mut self, index: SArenaIdx<T>) -> Option<&mut T>{
         if let ArenaCell::Allocated{val, generation} = &mut self.cells[index.index]{
             if *generation == index.generation{
                 Some(val)
@@ -361,9 +289,6 @@ impl<T> Arena<T>{
         }
     }
 
-    ///
-    /// Returns an optional mutable reference to the value of a cell at a index with any generation.
-    ///
     pub fn get_any_mut(&mut self, index: usize) -> Option<&mut T>{
         if let ArenaCell::Allocated{val, generation: _} = &mut self.cells[index]{
             Some(val)
@@ -380,7 +305,7 @@ impl<T> Arena<T>{
     ///```rust
     /// use gen_arena::*;
     ///
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     /// let i2 = arena.insert(2);
@@ -395,7 +320,7 @@ impl<T> Arena<T>{
     ///
     ///```
     ///
-    pub fn get2_mut(&mut self, indices: (ArenaIdx<T>, ArenaIdx<T>)) -> (Option<&mut T>, Option<&mut T>){
+    pub fn get2_mut(&mut self, indices: (SArenaIdx<T>, SArenaIdx<T>)) -> (Option<&mut T>, Option<&mut T>){
         if indices.0.index == indices.1.index{
             if indices.0.generation == indices.1.generation{
                 panic!("Cannot take 2 mutable references to a value at the same index.")
@@ -457,7 +382,7 @@ impl<T> Arena<T>{
     ///
     /// ```rust
     /// use gen_arena::*;
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     /// let i2 = arena.insert(1);
@@ -480,7 +405,7 @@ impl<T> Arena<T>{
     ///
     /// ```rust
     /// use gen_arena::*;
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     /// let i2 = arena.insert(2);
@@ -504,9 +429,10 @@ impl<T> Arena<T>{
     ///
     /// Returns an iterator over the Allocated cells with index.
     ///
-    /// ```rust
+    /// TODO: either add new iterator type for SArena or use ArenaIdx for SArena.
+    /// ```rust, ignore
     /// use gen_arena::*;
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     /// let i2 = arena.insert(2);
@@ -534,7 +460,7 @@ impl<T> Arena<T>{
     ///
     /// ```rust
     /// use gen_arena::*;
-    /// let mut arena = Arena::new();
+    /// let mut arena = SArena::<_, 100>::new();
     ///
     /// let i1 = arena.insert(1);
     /// let i2 = arena.insert(2);
@@ -556,13 +482,8 @@ impl<T> Arena<T>{
     }
 
     #[inline]
-    pub fn reserve(&mut self, additional: usize){
-        self.cells.reserve(additional)
-    }
-
-    #[inline]
     pub fn capacity(&self) -> usize{
-        self.cells.capacity()
+        N
     }
 
     #[inline]
@@ -570,110 +491,3 @@ impl<T> Arena<T>{
         self.num
     }
 }
-
-impl<T> Index<ArenaIdx<T>> for Arena<T>{
-    type Output = T;
-
-    fn index(&self, index: ArenaIdx<T>) -> &Self::Output {
-        self.get(index).expect("There is no element at this index with that generation.")
-    }
-}
-
-impl<T> IndexMut<ArenaIdx<T>> for Arena<T>{
-    fn index_mut(&mut self, index: ArenaIdx<T>) -> &mut Self::Output {
-        self.get_mut(index).expect("There is no element at this index with that generation.")
-    }
-}
-
-pub struct Enumerator<'i, T: 'i>{
-    pub(crate) iter: std::iter::Enumerate<std::slice::Iter<'i, ArenaCell<T>>>,
-}
-
-impl<'i, T> Iterator for Enumerator<'i, T>{
-    type Item = (ArenaIdx<T>, &'i T);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop{
-            match self.iter.next(){
-                Some((_, ArenaCell::Freed{..})) => continue,
-                Some((i, ArenaCell::Allocated{val, generation})) => {
-                    return Some((ArenaIdx::new(i, *generation), val));
-                }
-                None => {return None;},
-            }
-        }
-    }
-}
-
-pub struct Iter<'i, T: 'i>{
-    pub (crate) iter: Enumerator<'i, T>,
-}
-
-impl<'i, T> Iterator for Iter<'i, T>{
-    type Item = &'i T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(|(_, val)|{val})
-    }
-}
-
-pub struct EnumeratorMut<'i, T: 'i>{
-    pub(crate) iter: std::iter::Enumerate<std::slice::IterMut<'i, ArenaCell<T>>>,
-}
-
-impl<'i, T> Iterator for EnumeratorMut<'i, T>{
-    type Item = (ArenaIdx<T>, &'i mut T);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop{
-            match self.iter.next(){
-                Some((_, ArenaCell::Freed{..})) => continue,
-                Some((i, ArenaCell::Allocated{val, generation})) => {
-                    return Some((ArenaIdx::new(i, *generation), val));
-                }
-                None => {return None;},
-            }
-        }
-    }
-}
-
-pub struct IterMut<'i, T: 'i>{
-    pub(crate) iter: EnumeratorMut<'i, T>,
-}
-
-impl<'i, T> Iterator for IterMut<'i, T>{
-    type Item = &'i mut T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter.next().map(|(_, val)|{val})
-    }
-}
-
-#[cfg(test)]
-mod test{
-    use super::*;
-    #[test]
-    fn test_allocation_deallocation(){
-        let mut arena = Arena::new();
-
-        let i0 = arena.insert(0);
-        let i1 = arena.insert(1);
-
-        assert_eq!(*arena.get(i0).unwrap(), 0);
-        assert_eq!(*arena.get(i1).unwrap(), 1);
-
-        arena.remove(i1);
-
-        assert_eq!(arena.get(i1), None);
-
-        let i2 = arena.insert(2);
-
-        assert_eq!(*arena.get(i2).unwrap(), 2);
-        assert_eq!(arena.get(i1), None);
-
-        arena.enumerate().for_each(|(index, val)|{
-            println!("{}, {}", index.index(), val);
-        });
-    }
-}
-
